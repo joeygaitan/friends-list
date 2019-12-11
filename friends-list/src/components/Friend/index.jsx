80d45413e58b55a7eb074abdadcb391097ac2956
Joey Gaitan <joeygaitan707@gmail.com>
2019-12-11 13:24:37 -0800

    adding in some css

--- friends-list/src/components/Friend/index.jsx
+++ friends-list/src/components/Friend/index.jsx
@@ -7,9 +7,16 @@ class Friend extends Component {
     }
     render() { 
         return ( 
-            <div>
-                
-            </div>
+            <>
+                <div className="card" style="width:400px">
+                    <img className="card-img-top" src="img_avatar1.png" alt="Card image"/>
+                    <div className="card-body">
+                        <h4 className="card-title">John Doe</h4>
+                        <p className="card-text">Some example text.</p>
+                        <a href="#" className="btn btn-primary">See Profile</a>
+                    </div>
+                </div>
+            </>
          );
     }
 }
