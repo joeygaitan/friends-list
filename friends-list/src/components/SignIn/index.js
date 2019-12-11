cd4957118035b9107da227f0a24d18b6db0d2012
Joey Gaitan <joeygaitan707@gmail.com>
2019-12-11 14:22:22 -0800

    added in browserRouter

--- friends-list/src/components/SignIn/index.js
+++ friends-list/src/components/SignIn/index.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
 
 class SignUp extends Component {
     constructor(props) {
         super(props);
-        this.state = { username:'',password:'', failed: false }
+        this.state = { username:'',password:'', failed: false, stillLogged: false }
     }
 
-    ifClicked=()=>{
-      const { username, password,failed} = this.state
-      if(username !='' && password!= ''){
-        console.log(username,password)
-        let userInfo = {userInfo,password}
-        sessionStorage.setItem('userInfo', userInfo)
-        this.history.push('/Dashboard')
+    ifClicked= () =>{
+      console.log(this.state.username,this.state.password)
+      if(this.state.username !=='' && this.state.password!== ''){
+        console.log(this.state.username,this.state.password)
+        let userInfo = {username:this.state.username,password:this.state.password}
+        if(!sessionStorage.getItem('userInfo')){
+          sessionStorage.setItem('userInfo', userInfo)
+        }else{
+
+        }
+        this.history.push('/dashboard')
       }else{
-        this.setState({ failed: !failed })
+        this.setState({ failed: !this.state.failed })
       }
     }
     render() { 
+      console.log(this.state.username,this.state.password)
         return (
             <div className="container">
               <div className='row'>
-              <h5>The Buddy System</h5>
+              <h1 className="col-lg-12" style={{"text-align":"center", "padding-top":"30px"}}>The Buddy System</h1>
               </div>
             <div className="row">
               <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
@@ -38,28 +45,27 @@ class SignUp extends Component {
                         onChange= { (e) => {
                         this.setState({ username: e.target.value})
                         }} 
-                        required autofocus/>
+                        required/>
                         <label for="inputEmail">Email address</label>
                       </div>
         
                       <div className="form-label-group">
-                        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
+                        <input type="password" id="inputPassword" className="form-control" placeholder="Password" onChange= { (e) => {
+                        this.setState({ password: e.target.value})
+                        }} required/>
                         <label for="inputPassword">Password</label>
                       </div>
         
                       <div className="custom-control custom-checkbox mb-3">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" 
-                        onChange= { (e) => {
-                        this.setState({ password: e.target.value})
-                        }}/>
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
                         <label className="custom-control-label" for="customCheck1">Remember password</label>
                       </div>
-                      <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit" onClick={this.ifClicked}>Sign in</button>
+                      <button className="btn btn-lg btn-primary btn-block text-uppercase" onClick={()=>this.ifClicked()}>Sign in</button>
 
-                      {this.state.failed ? <h4 className=""style="color:red">user name or password is missing please try again</h4> : ""}
+                      {this.state.failed ? <h4 className="" style={{"color":"red"}}>user name or password is missing please try again</h4> : ""}
                       <hr className="my-4"/>
                       <h5 className="card-title text-center">If you are new here put in a desired username and password. Then please press new Here</h5>
-                      <button className="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i className="fab fa-google mr-2" onClick={this.ifClicked}></i>New Here</button>
+                      <button className="btn btn-lg btn-google btn-block text-uppercase"><i className="fab fa-google mr-2" onClick={()=>this.ifClicked()}></i>New Here</button>
                     </form>
                   </div>
                 </div>
@@ -70,4 +76,4 @@ class SignUp extends Component {
     }
 }
  
-export default SignUp;
+export default withRouter(SignUp);
